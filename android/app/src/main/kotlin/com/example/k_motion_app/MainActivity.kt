2f0247754662ae70f7cae351ec7a2bb2d53a3680
naneps <ekaprasetya2244@gmail.com>
2024-04-03 15:18:23 +0700

    Update code formatting

---
+++ android/app/src/main/kotlin/com/example/k_motion_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.k_motion_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,26 @@
+part of 'app_pages.dart';
+// DO NOT EDIT. This is code generated via package:get_cli/get_cli.dart
+
+abstract class Routes {
+  Routes._();
+  static const HOME = _Paths.HOME;
+  static const CORE = _Paths.CORE;
+  static const PROFILE = _Paths.PROFILE;
+  static const NOTIFICATION = _Paths.NOTIFICATION;
+  static const ORDER = _Paths.ORDER;
+  static const CART = _Paths.CART;
+  static const SETTING = _Paths.SETTING;
+  static const AUTH = _Paths.AUTH;
+}
+
+abstract class _Paths {
+  _Paths._();
+  static const HOME = '/home';
+  static const CORE = '/core';
+  static const PROFILE = '/profile';
+  static const NOTIFICATION = '/notification';
+  static const ORDER = '/order';
+  static const CART = '/cart';
+  static const SETTING = '/setting';
+  static const AUTH = '/auth';
+}
